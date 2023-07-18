fdcd98cd84bfe2753ef1cba26f69d4285af715ea
Naiyyar <naiyyar.abbas512013@gmail.com>
2023-07-18 11:47:05 +0530

    querying data

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,10 +3,7 @@
 module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
-    field :user_id, Integer, null: false
-    field :title, String
-    field :body, String
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :title, String, null: false
+    field :body, String, null: true
   end
 end
